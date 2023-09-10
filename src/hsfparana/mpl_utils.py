73a51e48d5e4d5f2c8de04dd5efef4e877533c95
Kilian Lieret <kilian.lieret@posteo.de>
2023-09-10 09:44:39 +0200

    Plots for IRIS-HEP retreat

--- src/hsfparana/mpl_utils.py
+++ src/hsfparana/mpl_utils.py
@@ -6,8 +6,10 @@ import matplotlib.dates as mdates
 from dateutil import parser as date_parser
 
 
-def set_date_xaxis(ax, start_date="01/01/2020"):
-    ax.set_xlim(date_parser.parse(start_date), datetime.datetime.now())
+def set_date_xaxis(ax, start_date="01/01/2020", end_date=None) -> None:
+    if end_date is None:
+        end_date = datetime.datetime.now()
+    ax.set_xlim(date_parser.parse(start_date), end_date)
     ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=(1,)))
     ax.xaxis.set_minor_locator(mdates.MonthLocator(bymonth=(7,)))
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%b\n%Y"))
